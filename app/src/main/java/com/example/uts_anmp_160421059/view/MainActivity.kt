9a0d55c50662597ad5dc93b65a08714994b3ddc2
Rivaldo <aldolauren18@gmail.com>
2024-04-16 17:49:07 +0700

    First Commit

---
+++ app/src/main/java/com/example/uts_anmp_160421059/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.uts_anmp_160421059.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.uts_anmp_160421059.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
